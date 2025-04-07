a5d546d3be413cc535908ad1971228ec8840f858
Eman <emanabukhater2001@gmail.com>
2025-04-07 23:25:19 +0300

    add some styles

--- firebase-auth-app/src/App.jsx
+++ firebase-auth-app/src/App.jsx
@@ -6,6 +6,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import PrivateRoute from "./components/PrivateRoute";
+import "./App.css";
 
 const App = () => {
   return (
